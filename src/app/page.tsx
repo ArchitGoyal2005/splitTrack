410352f3a09eced0dfe585dac879594673f35d17
Sajal <23f2002548@ds.study.iitm.ac.in>
2024-07-17 22:01:25 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <div className="container mx-auto px-4 sm:px-8">hi</div>
+      </div>
+    </>
+  );
 }

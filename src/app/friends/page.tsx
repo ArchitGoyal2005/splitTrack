bd5d904dc1ae4049c36865e26baac76dd485020d
ArchitGoyal2005 <architgoyal2005@gmail.com>
2024-07-28 00:59:34 +0530

    Created layout of friends page

---
+++ src/app/friends/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <div className="grid grid-cols-[8fr,2fr] gap-4 grid-rows-2 w-full h-full">
+      <div className="bg-slate-50 rounded-3xl row-span-2 w-full h-full grid">
+        <div></div>
+      </div>
+      <div className="bg-slate-50 rounded-3xl w-full h-full ">
+        <div></div>
+      </div>
+      <div className="bg-slate-50 rounded-3xl w-full h-full ">
+        <div></div>
+      </div>
+    </div>
+  );
+}
